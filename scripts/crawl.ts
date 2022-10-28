ed938a067d7cfd8c220d6300c50b1425c1ce64eb
Changwan Jun <wan2land@gmail.com>
2022-10-28 23:44:29 +0900

    feat: add 2004~2023 cals

--- scripts/crawl.ts
+++ scripts/crawl.ts
@@ -1,32 +1,35 @@
 import { DateInfo, getDateInfos } from "../api.ts";
 
-const apiKey = Deno.env.get("API_KEY") ?? "";
-if (Deno.args.length === 0) {
-  console.log("Usage: crawl.ts <year>");
-  Deno.exit(1);
-}
-const year = +Deno.args[0];
+async function generateDateFile(year: number) {
+  const dates = new Map<string, DateInfo>();
+  const types = [
+    "getHoliDeInfo",
+    "getRestDeInfo",
+    "getAnniversaryInfo",
+    "get24DivisionsInfo",
+    "getSundryDayInfo",
+  ] as const;
 
-const dates = new Map<string, DateInfo>();
-const types = [
-  "getHoliDeInfo",
-  "getRestDeInfo",
-  "getAnniversaryInfo",
-  "get24DivisionsInfo",
-  "getSundryDayInfo",
-] as const;
-
-for (const type of types) {
-  for (const date of await getDateInfos(apiKey, type, year)) {
-    dates.set(JSON.stringify(date), date);
+  for (const type of types) {
+    for (const date of await getDateInfos(apiKey, type, year)) {
+      dates.set(JSON.stringify(date), date);
+    }
   }
+
+  const sorted = Array.from(dates.values()).sort((a, b) => {
+    return a.date.localeCompare(b.date);
+  });
+
+  Deno.writeFile(
+    `${year}.json`,
+    new TextEncoder().encode(JSON.stringify(sorted, null, 2)),
+  );
+  console.log(`Generated ${year}.json`);
 }
 
-const sorted = Array.from(dates.values()).sort((a, b) => {
-  return a.date.localeCompare(b.date);
-});
+const apiKey = Deno.env.get("API_KEY") ?? "";
 
-Deno.writeFile(
-  `${year}.json`,
-  new TextEncoder().encode(JSON.stringify(sorted, null, 2)),
-);
+const year = new Date().getFullYear();
+
+await generateDateFile(year);
+await generateDateFile(year + 1);
